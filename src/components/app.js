c0584395d30d09fbdfbc1d5e0874080997d9b91b
Code-JD <jonathan.d.herring@gmail.com>
2020-01-06 15:56:28 -0800

    third commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import moment from "moment";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 import NavigationContainer from './navigation/navigation-container';
@@ -13,19 +8,19 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='container'>
         <Router>
           <div>
-          <h1>Jonathan Herring's Portfolio</h1>
-          <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
             <NavigationContainer />
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route exact path="/auth" component={Auth} />
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />

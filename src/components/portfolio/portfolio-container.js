c85117beaab5c64e21e138ff610f7e51aa62325b
Code-JD <jonathan.d.herring@gmail.com>
2020-01-06 08:18:30 -0800

    second commit

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 import PortfolioItem from "./portfolio-item";
 
@@ -9,15 +10,11 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my portfolio",
             isLoading: false,
-            data: [
-                { title: "Quip", category: "eCommerce" },
-                { title: "Eventbrite", category: "Scheduling" },
-                { title: "Ministry Safe", category: "Enterprise" },
-                { title: "SwingAway" , category: "eCommerce"}
-            ]
+            data: []
         };
 
         this.handleFilter = this.handleFilter.bind(this);
+        
     }
 
     handleFilter(filter) {
@@ -29,12 +26,29 @@ export default class PortfolioContainer extends Component {
 
     }
 
+    getPortfolioItems() {
+        axios
+          .get("https://jonathanherring.devcamp.space/portfolio/portfolio_items")
+          .then(response => {
+            this.setState({
+                data: response.data.portfolio_items
+            })
+          })
+          .catch(error => {
+            // handle error
+            console.log(error);
+          });
+    }
+
     portfolioItems() {
         return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} />;
+            return (<PortfolioItem key={item.id} item={item} />);
         });
     }
 
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
 
     render() {
         if (this.state.isLoading) {

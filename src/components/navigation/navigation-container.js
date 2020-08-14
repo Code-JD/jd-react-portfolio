a07c7b611892ab98ef2ed3f5c64f39ddabf77456
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-08-13 17:47:23 -0700

    changed colors and contact page

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -6,38 +6,38 @@ import { NavLink } from "react-router-dom";
 
 const NavigationComponent = props => {
   const dynamicLink = (route, linkText) => {
-    return (
-      <div className="nav-link-wrapper">
-        <NavLink to={route} activeClassName="nav-link-active">
-          {linkText}
-        </NavLink>
-      </div>
-    );
+	return (
+	  <div className="nav-link-wrapper">
+		<NavLink to={route} activeClassName="nav-link-active">
+		  {linkText}
+		</NavLink>
+	  </div>
+	);
   };
 
   const handleSignOut = () => {
-    axios
-      .delete("https://api.devcamp.space/logout", { withCredentials: true })
-      .then(response => {
-        if (response.status === 200) {
-          props.history.push("/");
-          props.handleSuccessfulLogout();
-        }
-        return response.data;
-      })
-      .catch(error => {
-        console.log("Error signing out", error);
-      });
+	axios
+	  .delete("https://api.devcamp.space/logout", { withCredentials: true })
+	  .then(response => {
+		if (response.status === 200) {
+		  props.history.push("/");
+		  props.handleSuccessfulLogout();
+		}
+		return response.data;
+	  })
+	  .catch(error => {
+		console.log("Error signing out", error);
+	  });
   };
 
   return (
-    <div className="nav-wrapper">
-      <div className="left-side">
-        <div className="nav-link-wrapper">
-          <NavLink exact to="/" activeClassName="nav-link-active">
-            Home
-          </NavLink>
-        </div>
+	<div className="nav-wrapper">
+	  <div className="left-side">
+		<div className="nav-link-wrapper">
+		  <NavLink exact to="/" activeClassName="nav-link-active">
+			Home
+		  </NavLink>
+		</div>
 
 {/* //         <div className="nav-link-wrapper">
 //           <NavLink to="/about-me" activeClassName="nav-link-active">
@@ -45,11 +45,11 @@ const NavigationComponent = props => {
 //           </NavLink>
 //         </div> */}
 
-        <div className="nav-link-wrapper">
-          <NavLink to="/contact" activeClassName="nav-link-active">
-            Contact
-          </NavLink>
-        </div>
+		<div className="nav-link-wrapper">
+		  <NavLink to="/contact" activeClassName="nav-link-active">
+			Contact
+		  </NavLink>
+		</div>
 
 {/* //         <div className="nav-link-wrapper">
 //           <NavLink to="/blog" activeClassName="nav-link-active">
@@ -57,20 +57,20 @@ const NavigationComponent = props => {
 //           </NavLink>
 //         </div> */}
 
-        {props.loggedInStatus === "LOGGED_IN" ? (
-          dynamicLink("/portfolio-manager", "Portfolio Manager")
-        ) : null}
-      </div>
+		{props.loggedInStatus === "LOGGED_IN" ? (
+		  dynamicLink("/portfolio-manager", "Portfolio Manager")
+		) : null}
+	  </div>
 
-      <div className="right-side">
-        Jonathan Herring
-        {props.loggedInStatus === "LOGGED_IN" ? (
-          <a onClick={handleSignOut}>
-            <FontAwesomeIcon icon="sign-out-alt" />
-          </a>
-        ) : null}
-      </div>
-    </div>
+	  <div className="right-side">
+		Jonathan Herring
+		{props.loggedInStatus === "LOGGED_IN" ? (
+		  <a onClick={handleSignOut}>
+			<FontAwesomeIcon icon="sign-out-alt" />
+		  </a>
+		) : null}
+	  </div>
+	</div>
   );
 };
 
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,57 +1,52 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaLinkedin, FaGithubSquare, FaEnvelope } from "react-icons/fa";
 import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
 
 export default function() {
   return (
     <div className="content-page-wrapper">
-      <div
-        className="left-column"
-        style={{
-          background: "url(" + contactPagePicture + ") no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}
-      />
-      <div className="right-column">
-        <div className="contact-bullet-points">
-          <div className="bullet-point-group">
-		  <div className="info-text">
-				<h1>I am currently looking for opportunities in Software Development.</h1>
-				<h1>Feel free to reach out and connect with me about future employment and Freelancing.</h1>
-		  </div>
-			<div className="icon">
-				{/* <FontAwesomeIcon icon="phone" /> */}
+		<div
+		className="left-column"
+		style={{
+			background: "url(" + contactPagePicture + ") no-repeat",
+			backgroundSize: "cover",
+			backgroundPosition: "center"
+		}}
+		/>
+		<div className="right-column">
+			<div className="contact-bullet-points">
+				<div className="bullet-point-group">
+					<div className="info-text">
+						<h1>I am currently looking for opportunities in Software Development.</h1>
+						<h1>Feel free to reach out and connect with me about future employment and Freelancing.</h1>
+					</div>
 				</div>
 
-				<div className="text">562-508-6236</div>
+				{/* <div className="contact-links"></div> */}
+
+				<div className="social-media">
+					<div className="linkedin">
+						<a href="https:/www.linkedin.com/in/jonathan-herring-code">
+							<FaLinkedin color="#ff4000" size='10rem' />
+						</a>
+					</div>
+					
+					<div className="github">
+						<a href="https://github.com/Code-JD">
+							<FaGithubSquare color="#ff4000" size='10rem' />
+						</a>
+					</div>
+
+					<div className="email">
+						<a href="mailto:jonathan.d.herring@gmail.com">
+							<FaEnvelope color="#ff4000" size='10rem' />
+						</a>
+					</div>
+				</div>  
+
 			</div>
-
-			<div className="bullet-point-group">
-				<div className="icon">
-				{/* <FontAwesomeIcon icon="envelope" /> */}
-				</div>
-
-				<div className="text">jonathan.d.herring@gmail.com</div>
-			</div>
-
-			<div className="bullet-point-group">
-				<div className="icon">
-				{/* <FontAwesomeIcon icon="map-marked-alt" /> */}
-				</div>
-
-				<div className="text">Long Beach, CA</div>
-			
-
-			<div className="icon">
-				{/* <FontAwesomeIcon icon="envelope" /> */}
-			</div>
-
-			<div className="contact-links"></div>
-
-          </div>
-        </div>
-      </div>
+		</div>
     </div>
   );
 }
